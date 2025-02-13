77e8ab69126d7726d405bc8c54bf62adf37e4a03
Alexxsaz <alexxsaz96@gmail.com>
2025-02-13 19:04:32 +0500

    First task completed

--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,10 +1,28 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => {
+builder.Services.AddControllers(options =>
+    {
+        options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+        options.ReturnHttpNotAcceptable = true;
+        options.RespectBrowserAcceptHeader = true;
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
     });
+
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"));
+}, Array.Empty<Assembly>());
 
 var app = builder.Build();
 
